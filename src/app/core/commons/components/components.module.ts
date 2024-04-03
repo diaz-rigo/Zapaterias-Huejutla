6e71ef5b53413e6a72dda1ba4a436d29f420c5f7
meandro02 <rd209422@gmail.com>
2024-04-03 16:12:24 -0600

    header

--- src/app/core/commons/components/components.module.ts
+++ src/app/core/commons/components/components.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { AnimateOnScrollModule } from 'primeng/animateonscroll';
 
-const COMPONENTS = [HeaderComponent,FooterComponent]; // Agrega los componentes
+const COMPONENTS = [HeaderComponent, FooterComponent]; // Agrega los componentes
+
+const MATERIALS__ = [AnimateOnScrollModule]; // Agrega los componentes
 
 @NgModule({
   declarations: [...COMPONENTS],
-  exports: [...COMPONENTS],
-  imports: [CommonModule],
+  exports: [...COMPONENTS, ...MATERIALS__],
+  imports: [CommonModule, ...MATERIALS__],
   providers: [],
 })
 export class CoreComponentsModule {}
--- src/app/core/commons/material/material.module.ts
+++ src/app/core/commons/material/material.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AnimateOnScrollModule } from 'primeng/animateonscroll';
 
 
+
+
+const MATERIALS__ = [AnimateOnScrollModule]; // Agrega los componentes
 
 @NgModule({
   declarations: [],
   imports: [
+    ...MATERIALS__,
     CommonModule
   ]
 })
