303d97add81f2f12bc302719bc6e7a98c44b6444
meandro02 <rd209422@gmail.com>
2024-04-03 17:51:31 -0600

    badge

--- src/app/core/commons/components/components.module.ts
+++ src/app/core/commons/components/components.module.ts
@@ -3,10 +3,10 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { AnimateOnScrollModule } from 'primeng/animateonscroll';
-
+import { BadgeModule } from 'primeng/badge';
 const COMPONENTS = [HeaderComponent, FooterComponent]; // Agrega los componentes
 
-const MATERIALS__ = [AnimateOnScrollModule]; // Agrega los componentes
+const MATERIALS__ = [AnimateOnScrollModule,BadgeModule]; // Agrega los componentes
 
 @NgModule({
   declarations: [...COMPONENTS],
--- src/app/core/commons/material/material.module.ts
+++ src/app/core/commons/material/material.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AnimateOnScrollModule } from 'primeng/animateonscroll';
+import { BadgeModule } from 'primeng/badge';
 
 
 
-
-const MATERIALS__ = [AnimateOnScrollModule]; // Agrega los componentes
+// const MATERIALS__ = [AnimateOnScrollModule,BadgeModule]; // Agrega los componentes
 
 @NgModule({
   declarations: [],
   imports: [
-    ...MATERIALS__,
+    // ...MATERIALS__,
     CommonModule
   ]
 })
