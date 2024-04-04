0321741d0fc75f1967ff8ff9903cb3c4bb2df098
anthony= <134122438+Nikrovikey@users.noreply.github.com>
2024-04-04 08:06:48 -0600

    home

--- src/app/features/public/public.module.ts
+++ src/app/features/public/public.module.ts
@@ -1,12 +1,12 @@
 import { PublicCommonsModule } from './commons/commons.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { CarouselModule } from 'primeng/carousel';
 import { PublicRoutingModule } from './public-routing.module';
 import { PublicComponent } from './public.component';
 import { HomeView } from './view/home/home.view';
 import { NotFoundView } from './view/not-found/not-found.view';
-
+import { ButtonModule } from 'primeng/button';
 import { DetailView } from './view/detail/detail.view';
 
 const VIEW___ = [DetailView,NotFoundView,HomeView]; // Agrega los componentes
@@ -15,13 +15,13 @@ const VIEW___ = [DetailView,NotFoundView,HomeView]; // Agrega los componentes
 
 @NgModule({
   declarations: [
-    PublicComponent,
+    PublicComponent, 
     ...VIEW___
   ],
   imports: [
     PublicCommonsModule,
-    CommonModule,
-    PublicRoutingModule
+    CommonModule,CarouselModule,
+    PublicRoutingModule, ButtonModule
   ]
 })
 export class PublicModule { }
--- src/app/features/public/view/home/home.view.ts
+++ src/app/features/public/view/home/home.view.ts
@@ -10,6 +10,46 @@ import { Router } from '@angular/router';
 export class HomeView {
 
 
+  responsiveOptions: any[] | undefined;
+
+  // constructor(private productService: ProductService) { }
+
+  ngOnInit() {
+    // this.productService.getProductsSmall().then((products) => {
+    //   this.products = products;
+    // });
+    this.images = [
+      'https://res.cloudinary.com/dvvhnrvav/image/upload/v1712184564/productMeanZapateriaHuejutla/marcas/v3t9d2fpfgfs5exx85hb.png',
+      'https://res.cloudinary.com/dvvhnrvav/image/upload/v1712184624/productMeanZapateriaHuejutla/marcas/tlwtmtsgfyw4w7djwpae.png',
+      'https://res.cloudinary.com/dvvhnrvav/image/upload/v1712184637/productMeanZapateriaHuejutla/marcas/odvtxdk2rdvehlyepyqo.png',
+      'https://res.cloudinary.com/dvvhnrvav/image/upload/v1712184735/productMeanZapateriaHuejutla/marcas/wjy5bx9rbaxmytyiyon5.png',
+      // 'https://via.placeholder.com/600x300',
+      // 'https://via.placeholder.com/600x300',
+      // 'https://via.placeholder.com/600x300'
+    ];
+    this.responsiveOptions = [
+      {
+        breakpoint: '1199px',
+        numVisible: 1,
+         numScroll: 1
+      },
+      {
+        breakpoint: '991px',
+        numVisible: 2,
+        numScroll: 1
+      },
+      {
+        breakpoint: '767px',
+        numVisible: 1,
+        numScroll: 1
+      }
+    ];
+  }
+
+
+
+  images!: string[];
+
 
   constructor(private router: Router) {}
 
