f93f7897a4f058c3418bec3e917eb2fc6a88bf6c
meandro02 <rd209422@gmail.com>
2024-04-30 21:19:18 -0600

    coneccion api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true
-  ,api:'http://localhost:3000'
-  // ,api:'https://za.com'
+  // ,api:'http://localhost:3000'
+  ,api:'https://zapateria-back.onrender.com'
 
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@
 
 export const environment = {
   production: false
-  ,api:'http://localhost:3000'
-  // ,api:'https://za.onrender.com'
+  // ,api:'http://localhost:3000'
+  ,api:'https://zapateria-back.onrender.com'
 };
 
